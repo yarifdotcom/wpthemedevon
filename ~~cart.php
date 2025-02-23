5aac2db5f7e91eca40681f468ec529a23a8481d5
adityarifa <yarifdotcom@gmail.com>
2025-02-23 20:45:48 +0700

    update varian show add cart and cart list

--- ~~cart.php
+++ ~~cart.php
@@ -16,7 +16,7 @@ Template Name: Cart Custom
      <section class="title-section ">
         <div class="container-fluid no-max">
             <div class="row justify-content-center text-center">
-                <div class="col col-sm-12 text20 serif text-center pt-25em pb-15em">
+                <div class="col col-sm-12 text20 serif text-center pt-25em pb-15em fs-13vw">
                     <h1><?php if(get_field('title_control')){echo get_field('title_control');} else { echo get_the_title();} ?></h1>
                 </div><!-- /col -->
             </div><!-- /row -->
